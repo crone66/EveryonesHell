4880ad1076fd8603d20156d235249be38bd7ec19
LukasBos <lukas.bosniak@gmail.com>
2016-11-09 17:04:08 +0100

    Added AnimationManager

---
+++ EveryonesHell/EveryonesHell/AnimationManager.cs
@@ -0,0 +1,79 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System.Collections.Generic;
+
+/* 
+ * Purpose: Abspielen von Animationen per Spritesheet
+ * Author: Lukas Bosniak
+ * Date: 9.11.2016
+ */
+
+namespace AnimationManager
+{
+    public class AnimationManager
+    {
+        private Sprite sprite;
+        private int countX, countY, width, height, currentFrame;
+        private float frameTime, currentanimationTime;
+        private List<IntRect> spriteList;
+
+        public AnimationManager(Sprite sprite, int spriteCountX, int spriteCountY, int frameWidth, int frameHeight, float animationFrameTime)
+        {
+            this.sprite = sprite;
+            countX = spriteCountX;
+            countY = spriteCountY;
+            width = frameWidth;
+            height = frameHeight;
+            frameTime = animationFrameTime;
+            currentFrame = 0;
+            currentanimationTime = 0.0f;
+            spriteList = new List<IntRect>();
+
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            int positionX = 0;
+            int positionY = 0;
+            IntRect rect = new IntRect();
+
+            for (int i = 0; i < countY; i++)
+            {
+                positionY = height * i;
+
+                for (int j = 0; j < countX; j++)
+                {
+                    positionX = width * j;
+
+                    rect = new IntRect(positionX, positionY, width, height);
+
+                    spriteList.Add(rect);
+                }
+            }
+        }
+
+        public void Update(Time gameTime)
+        {
+            currentanimationTime += gameTime.AsMilliseconds();
+
+            if (currentanimationTime >= frameTime)
+            {
+                currentanimationTime = 0.0f;
+                currentFrame++;
+
+                if (currentFrame >= spriteList.Count)
+                {
+                    currentFrame = 0;
+                }
+            }
+
+            sprite.TextureRect = spriteList[currentFrame];
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            window.Draw(sprite);
+        }
+    }
+}
