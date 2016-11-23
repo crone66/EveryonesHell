f5eba25358faa0c997d2e25fd8214b1a5ef719cb
crone66 <marcel.croonenbroeck@gmail.com>
2016-11-24 00:14:43 +0100

    moved all gameplay related code to Scene, added debugging console

--- EveryonesHell/EveryonesHell/Game.cs
+++ EveryonesHell/EveryonesHell/Game.cs
@@ -20,14 +20,10 @@ namespace EveryonesHell
         private uint windowWidth = 800;
         private uint windowHeight = 600;
 
-        private ContentManager content;
-        private StreamedTileMap tileMap;
-        private Dictionary<int, Sprite> sprites;
-        private int x = 10;
-        private int y = 10;
-
         private View view;
-        private float zoomFactor = 2f;
+        private float zoomFactor = 1f;
+
+        private Scene scene;
 
         public Game()
         {
@@ -35,7 +31,6 @@ namespace EveryonesHell
             view = window.GetView();
             view.Zoom(zoomFactor);
             window.SetView(view);
-            
             LoadContent();
         }
 
@@ -45,109 +40,78 @@ namespace EveryonesHell
         public void Run()
         {
             Clock clock = new Clock();
-            while (window.IsOpen && !GlobalReferences.Exit)
+            while (window.IsOpen && GlobalReferences.State != GameState.Exit)
             {
+                window.DispatchEvents();
                 Time time = clock.Restart();
 
-                Input(time.AsSeconds());
-                Update(time.AsSeconds());
-                Draw(time.AsSeconds());
+                Input(time.AsMilliseconds());
+                Update(time.AsMilliseconds());
+                Draw(time.AsMilliseconds());
             }
         }
 
         private void LoadContent()
         {
-            
-
-            //Load your content
-            content = new ContentManager();
-            content.Add("Content/testBlue.png", new Texture("Content/testBlue.png"));
-            content.Add("Content/testRed.png", new Texture("Content/testRed.png"));
-            content.Add("Content/testGreen.png", new Texture("Content/testGreen.png"));
-            content.Add("Content/testGray.png", new Texture("Content/testGray.png"));
-            Sprite blue = content.CreateSprite("Content/testBlue.png");
-            Sprite red = content.CreateSprite("Content/testRed.png");
-            Sprite green = content.CreateSprite("Content/testGreen.png");
-            Sprite gray = content.CreateSprite("Content/testGray.png");
-
-            sprites = new Dictionary<int, Sprite>();
-            sprites.Add(0, green);
-            sprites.Add(1, blue);
-            sprites.Add(2, gray);
-            sprites.Add(-1, red);
-            TileMapGenerator generator = new TileMapGenerator();
-            tileMap = generator.GenerateMap(new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f, LayerDepth.One),
-                new AreaSpread[2] { new AreaSpread(1, 0.30f, 0, 20, 250, true, SpreadOption.Circle), new AreaSpread(2, 0.125f, 0, 20, 200, true, SpreadOption.Circle) }, 1, 1);
-
+            scene = new Scene(zoomFactor);
+            scene.LoadContent(window);
+            GlobalReferences.State = GameState.Play;
         }
 
-        private float elapsedTime;
-        private void Input(float elapsedSeconds)
+       
+        private void Input(float elapsedMilliseconds)
         {
-            elapsedTime -= elapsedSeconds;
-            if (elapsedTime <= 0f)
+            switch(GlobalReferences.State)
             {
-                elapsedTime = 0.15f;
-                //Handle input
-                if (Keyboard.IsKeyPressed(Keyboard.Key.W))
-                {
-                    TryMove(0, -1);
-                }
-                else if (Keyboard.IsKeyPressed(Keyboard.Key.S))
-                {
-                    TryMove(0, 1);
-                }
-                else if (Keyboard.IsKeyPressed(Keyboard.Key.A))
-                {
-                    TryMove(-1, 0);
-                }
-                else if (Keyboard.IsKeyPressed(Keyboard.Key.D))
-                {
-                    TryMove(1, 0);
-                }
+                case GameState.Play:
+                    scene.Input(elapsedMilliseconds);
+                    break;
+                case GameState.Pause:
+                    break;
+                case GameState.Menu:
+                    break;
+                case GameState.Exit:
+                    break;
             }
         }
 
-        private void TryMove(int xTranslation, int yTranslation)
-        {
-            int tileId = tileMap.GetTile(y + yTranslation, x + xTranslation);
-            if(tileId == 0)
-            {
-                x += xTranslation;
-                y += yTranslation;
-            }
-        }
-
-        private void Update(float elapsedSeconds)
+        private void Update(float elapsedMilliseconds)
         {
             //Update something
-            tileMap.Update(y, x);
+            switch(GlobalReferences.State)
+            {
+                case GameState.Play:
+                    scene.Update(elapsedMilliseconds);
+                    break;
+                case GameState.Pause:
+                    break;
+                case GameState.Menu:
+                    break;
+                case GameState.Exit:
+                    break;
+            }
         }
 
-        private void Draw(float elapsedSeconds)
+        private void Draw(float elapsedMilliseconds)
         {
-            window.Clear(Color.Blue);
-            float zoomWidth = ((windowWidth * zoomFactor) - (windowWidth)) / 2f;
-            float zoomHeight = ((windowHeight * zoomFactor) - (windowHeight)) / 2f;
 
-            int[,] fieldInView = tileMap.GetTileMapInScreen(Convert.ToInt32(windowWidth * zoomFactor), Convert.ToInt32(windowHeight * zoomFactor));
-            int rowCount = fieldInView.GetUpperBound(0);
-            int columnCount = fieldInView.GetUpperBound(1);
-            for (int row = 0; row < rowCount; row++)
+            switch (GlobalReferences.State)
             {
-                for (int column = 0; column < columnCount; column++)
-                {
-                    int id = fieldInView[row, column];
-                    Sprite sprite = sprites[id];
-                    sprite.Position = new Vector2f((column * 50) - zoomWidth, (row * 50) - zoomHeight);
-                    window.Draw(sprite);
-                }
+                case GameState.Play:
+                    {
+                        window.Clear(Color.Blue);
+                        scene.Draw(window);
+                        window.Display();
+                    }
+                    break;
+                case GameState.Pause:
+                    break;
+                case GameState.Menu:
+                    break;
+                case GameState.Exit:
+                    break;
             }
-
-            sprites[-1].Position = new Vector2f(((columnCount / 2) * 50) - zoomWidth, ((rowCount / 2) * 50) - zoomHeight);
-            window.Draw(sprites[-1]);
-
-            window.Display();
+            
         }
 
         public void Dispose()
--- EveryonesHell/EveryonesHell/Scene.cs
+++ EveryonesHell/EveryonesHell/Scene.cs
@@ -1,12 +1,157 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TileMapSystem;
 
 namespace EveryonesHell
 {
-    class Scene
+    public class Scene
     {
+        private TileMapManager mapManager;
+        private GeneratorSettings settings;
+        private AreaSpread[] areaSpreads;
+
+        private ContentManager content;
+        private Dictionary<int, Sprite> sprites;
+
+        private DebugConsoleManager consoleManager;
+
+        private byte[] fieldsInView;
+        private float elapsedTime;
+        private int x = 10;
+        private int y = -210; 
+        private float zoomFactor;
+        private uint windowWidth = 800;
+        private uint windowHeight = 600;
+
+
+        public Scene(float zoomFactor)
+        {
+            this.zoomFactor = zoomFactor;
+            settings = new GeneratorSettings(1, 50, 1.5f, 1000000, 10000000, true, 1000f);
+            areaSpreads = new AreaSpread[2]
+            {
+                new AreaSpread(1, 0.30f, 0, 20, 250, true, true, 5, SpreadOption.Circle, LayerType.Height),
+                new AreaSpread(2, 0.125f, 0, 20, 200, true, true, 5, SpreadOption.Circle, LayerType.Height)
+            };
+
+            mapManager = new TileMapManager(settings, areaSpreads, x, y);
+        }
+
+        public void LoadContent(RenderWindow window)
+        {
+            Font font = new Font(@"C:\Windows\Fonts\arial.ttf");
+            consoleManager = new DebugConsoleManager(this, font);
+            window.TextEntered += consoleManager.TextEntered;
+
+            //Load your content
+            content = new ContentManager();
+            content.Add("Content/testBlue.png", new Texture("Content/testBlue.png"));
+            content.Add("Content/testRed.png", new Texture("Content/testRed.png"));
+            content.Add("Content/testGreen.png", new Texture("Content/testGreen.png"));
+            content.Add("Content/testGray.png", new Texture("Content/testGray.png"));
+            Sprite blue = content.CreateSprite("Content/testBlue.png");
+            Sprite red = content.CreateSprite("Content/testRed.png");
+            Sprite green = content.CreateSprite("Content/testGreen.png");
+            Sprite gray = content.CreateSprite("Content/testGray.png");
+
+            sprites = new Dictionary<int, Sprite>();
+            sprites.Add(0, green);
+            sprites.Add(1, blue);
+            sprites.Add(2, gray);
+            sprites.Add(-1, red);
+        }
+
+        public void Update(float elapsedMilliseconds)
+        {
+            mapManager.Update(y, x);
+            fieldsInView = mapManager.CurrentLevel.GetTileMapInScreen(Convert.ToInt32(windowWidth * zoomFactor), Convert.ToInt32(windowHeight * zoomFactor));
+            consoleManager.Update(elapsedMilliseconds);
+        }
+
+        public void Input(float elapsedMilliseconds)
+        {
+            switch (GlobalReferences.State)
+            {
+                case GameState.Play:
+                    {
+                        if (!consoleManager.DebugConsole.IsOpen)
+                        {
+                            elapsedTime -= elapsedMilliseconds;
+                            if (elapsedTime <= 0f)
+                            {
+                                //Handle input
+                                if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+                                {
+                                    TryMove(0, -1);
+                                    elapsedTime = 15f;
+                                }
+                                else if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+                                {
+                                    TryMove(0, 1);
+                                    elapsedTime = 15f;
+                                }
+                                else if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+                                {
+                                    TryMove(-1, 0);
+                                    elapsedTime = 15f;
+                                }
+                                else if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+                                {
+                                    TryMove(1, 0);
+                                    elapsedTime = 15f;
+                                }
+                                else if (Keyboard.IsKeyPressed(Keyboard.Key.BackSlash))
+                                    consoleManager.DebugConsole.Open();
+                            }
+                        }
+                    }
+                    break;
+                case GameState.Pause:
+                    break;
+            }
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            windowWidth = Convert.ToUInt32(window.DefaultView.Size.X);
+            windowHeight = Convert.ToUInt32(window.DefaultView.Size.Y);
+
+            float zoomWidth = ((windowWidth * zoomFactor) - (windowWidth)) / 2f;
+            float zoomHeight = ((windowHeight * zoomFactor) - (windowHeight)) / 2f;
+
+            int rowCount = Convert.ToInt32((windowHeight * zoomFactor) / settings.TileSize);
+            int columnCount = Convert.ToInt32((windowWidth * zoomFactor) / settings.TileSize);
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int column = 0; column < columnCount; column++)
+                {
+                    int id = fieldsInView[(row * columnCount) + column];
+                    Sprite sprite = sprites[id];
+                    sprite.Position = new Vector2f((column * 50) - zoomWidth, (row * 50) - zoomHeight);
+                    window.Draw(sprite);
+                }
+            }
+
+            sprites[-1].Position = new Vector2f(((columnCount / 2) * 50) - zoomWidth, ((rowCount / 2) * 50) - zoomHeight);
+            window.Draw(sprites[-1]);
+
+            consoleManager.Draw(window);
+        }
+
+        private void TryMove(int xTranslation, int yTranslation)
+        {
+            int tileId = mapManager.CurrentLevel.GetTile(y + yTranslation, x + xTranslation);
+            if (tileId == 0)
+            {
+                x += xTranslation;
+                y += yTranslation;
+            }
+        }
     }
 }
