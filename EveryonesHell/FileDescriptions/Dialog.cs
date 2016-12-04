d05002e70d7d682b48ae68d87c04f8b8c32a92a5
Riku <Riku@RIKU-PC>
2016-12-05 00:36:40 +0100

    Added Dialog Struct and dialogcollection

--- EveryonesHell/FileDescriptions/Dialog.cs
+++ EveryonesHell/FileDescriptions/Dialog.cs
@@ -8,5 +8,24 @@ namespace FileDescriptions
 {
     public struct Dialog
     {
+        public int DialogID;
+        public string DialogText;
+        public int NextDialogId;
+        public string[] DialogAnswers;
+        public int[] DialogConditions;
+        public string[] DialogValues;
+        public int[] NextDialogAnswerIds;
+
+        public Dialog(int dialogid, string dialogtext, int nextdialogid, string[] dialoganswers, int[] dialogconditions, string[] dialogvalues, int[] nextdialoganswerids)
+        {
+            DialogID = dialogid;
+            DialogText = dialogtext;
+            NextDialogId = nextdialogid;
+            DialogAnswers = dialoganswers;
+            DialogConditions = dialogconditions;
+            DialogValues = dialogvalues;
+            NextDialogAnswerIds = nextdialoganswerids;
+        }
+
     }
 }
--- EveryonesHell/FileDescriptions/DialogCollection.cs
+++ EveryonesHell/FileDescriptions/DialogCollection.cs
@@ -8,5 +8,11 @@ namespace FileDescriptions
 {
     public struct DialogCollection
     {
+        public Dialog[] Dialogs;
+
+        public DialogCollection(Dialog[] dialogs)
+        {
+            Dialogs = dialogs;
+        }
     }
 }
