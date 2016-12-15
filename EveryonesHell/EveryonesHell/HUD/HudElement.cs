a3775afeb082a350f39ff9b658df6cc02d3388b9
crone66 <marcel.croonenbroeck@gmail.com>
2016-12-15 14:55:35 +0100

    Added HudMnager and HudElement

---
+++ EveryonesHell/EveryonesHell/HUD/HudElement.cs
@@ -0,0 +1,17 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace EveryonesHell.HUD
+{
+    public abstract class HudElement
+    {
+        public HudElement()
+        {
+
+        }
+
+        public abstract void Update(float elapsedSeconds);
+
+        public abstract void Draw(RenderWindow window);
+    }
+}
