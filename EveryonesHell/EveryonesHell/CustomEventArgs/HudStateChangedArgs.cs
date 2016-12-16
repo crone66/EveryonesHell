4c317aeacf9eaa0f5b8a777f58b53663e2846eeb
crone66 <marcel.croonenbroeck@gmail.com>
2016-12-16 20:41:02 +0100

    Moved custom event args into a subdir

---
+++ EveryonesHell/EveryonesHell/CustomEventArgs/HudStateChangedArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EveryonesHell
+{
+    public class HudStateChangedArgs : EventArgs
+    {
+        public bool PreviouseState;
+
+        public HudStateChangedArgs(bool prevState)
+        {
+            PreviouseState = prevState;
+        }
+    }
+}
