af9491dd69ad6ef825416b452f9a035f5da29d21
crone66 <marcel.croonenbroeck@gmail.com>
2016-12-18 16:09:57 +0100

    added ammo and reduced healtbar size

--- EveryonesHell/EveryonesHell/EntityManagment/Player.cs
+++ EveryonesHell/EveryonesHell/EntityManagment/Player.cs
@@ -11,10 +11,12 @@ namespace EveryonesHell.EntityManagment
         private Vector2f lastDirection;
         private HUD.DialogSystem dialog;
         private bool jetpackActive = false;
-        private Entity lastCollisionObject;
+        private InteractiveObject lastCollisionObject;
         private float elapsedTime, delay;
-
         private QuestManagment.QuestTracker questTracker;
+        private int ammo;
+        private int maxAmmo;
+        private Gaugebar ammunitionBar;
 
         public bool JetpackActive
         {
@@ -36,7 +38,7 @@ namespace EveryonesHell.EntityManagment
         /// <param name="size">Size of the player</param>
         /// <param name="sprite">Sprite of the player</param>
         /// <param name="dialog">Dialog system</param>
-        public Player(Vector2f position, Vector2i size, Sprite sprite, HUD.DialogSystem dialog, Gaugebar healthBar, int groupID, QuestManagment.QuestTracker questTracker, int factionId)
+        public Player(Vector2f position, Vector2i size, Sprite sprite, HUD.DialogSystem dialog, Gaugebar healthBar, Gaugebar ammunition, int groupID, QuestManagment.QuestTracker questTracker, int factionId)
             :base(position, size, new InventorySystem.Inventory(32), new AnimationManager(sprite, 3, 4, 50, 50, 0.16f), true, new Vector2f(1, 0), 620, healthBar, groupID, factionId, null)
         {
             lastDirection = new Vector2f(0, 0);
@@ -45,9 +47,13 @@ namespace EveryonesHell.EntityManagment
             OnShoot += Player_OnShoot;
             this.questTracker = questTracker;
             questTracker.inventory = Inventory;
+            questTracker.OnQuestFinished += QuestTracker_OnQuestFinished;
             delay = 3.0f;
-
+            ammo = 100;
+            maxAmmo = 100;
             OnCollision += InteractiveObject_OnCollision;
+            ammunitionBar = ammunition;
+            ammunitionBar.Init(ammo, maxAmmo);
         }
 
         /// <summary>
@@ -58,7 +64,7 @@ namespace EveryonesHell.EntityManagment
         /// <param name="size">Size of the player</param>
         /// <param name="sprite">Sprite of the player</param>
         /// <param name="dialog">Dialog system</param>
-        public Player(int tileRow, int tileColumn, Vector2i size, Sprite sprite, HUD.DialogSystem dialog, Gaugebar healthBar, int groupID, QuestManagment.QuestTracker questTracker, int factionId)
+        public Player(int tileRow, int tileColumn, Vector2i size, Sprite sprite, HUD.DialogSystem dialog, Gaugebar healthBar, Gaugebar ammunition, int groupID, QuestManagment.QuestTracker questTracker, int factionId)
             : base(tileRow, tileColumn, size, new InventorySystem.Inventory(32), new AnimationManager(sprite, 3, 4, 50, 50, 0.16f), true, new Vector2f(1, 0), 620, healthBar, groupID, factionId, null)
         {
             lastDirection = new Vector2f(0, 0);
@@ -67,17 +73,38 @@ namespace EveryonesHell.EntityManagment
             OnShoot += Player_OnShoot;
             this.questTracker = questTracker;
             questTracker.inventory = Inventory;
+            questTracker.OnQuestFinished += QuestTracker_OnQuestFinished;
             delay = 3.0f;
-
+            ammo = 100;
+            maxAmmo = 100;
             OnCollision += InteractiveObject_OnCollision;
+            ammunitionBar = ammunition;
+            ammunitionBar.Init(ammo, maxAmmo);
+        }
+
+        private void QuestTracker_OnQuestFinished(object sender, EventArgs e)
+        {
+            ammo += 100;
+            maxAmmo += 100;
+            if (maxAmmo > ammo)
+                ammo = maxAmmo;
+
+            ammunitionBar.Update(ammo, maxAmmo);
         }
 
         private void Dialog_OnDialogChanged(object sender, DialogChangedArgs e)
         {
             if (GlobalReferences.MainGame.CurrentScene.Quests.Quests.ToList().Exists(q => q.BasedOnDialogue == e.PrevDialogId))
-            {
+            {                
                 FileDescriptions.Quest quest = GlobalReferences.MainGame.CurrentScene.Quests.Quests.First(q => q.BasedOnDialogue == e.PrevDialogId);
-                questTracker.ActivateQuest(quest.QuestID);
+                if(questTracker.ActivateQuest(quest.QuestID))
+                    lastCollisionObject.RemoveDialog();
+            }
+
+            if(e.NewDialogId == -1)
+            {
+                lastCollisionObject.Freeze = false;
+                lastCollisionObject = null;
             }
         }
 
@@ -98,11 +125,9 @@ namespace EveryonesHell.EntityManagment
             else if (e.CollisionObjectSource != null && e.CollisionObjectSource != this)
                 otherObject = e.CollisionObjectSource;
 
-            if (otherObject != null)
+            if (otherObject != null && lastCollisionObject == null && otherObject is InteractiveObject)
             {
-                lastCollisionObject = otherObject;
-                
-                //Interaction key pressed?
+                lastCollisionObject = otherObject as InteractiveObject;
             }
         }
 
@@ -151,15 +176,28 @@ namespace EveryonesHell.EntityManagment
         /// <param name="e">Command arguments</param>
         public void OnAction(object sender, ExecuteCommandArgs e)
         {
-            if (lastCollisionObject != null && lastCollisionObject is InteractiveObject)
+            if (lastCollisionObject != null)
             {
-                InteractiveObject io = lastCollisionObject as InteractiveObject;
-                dialog.Open(io.GetDialog());
+                lastCollisionObject.Freeze = true;
+                dialog.Open(lastCollisionObject.GetDialog());
             }
             //dialog.Open(0);//, Position + (new Vector2f(Size.X / 2, Size.Y / 2)));
             //TODO: Action tile location
             // => Get object on action tile
             // => if object exists Execute Action 
+        }
+
+        public override void OnAttack(object sender, ExecuteCommandArgs e)
+        {
+            if (elaspedAttackTime > attackDelay)
+            {
+                if (ammo > 0)
+                {
+                    ammo--;
+                    ammunitionBar.Update(ammo, maxAmmo);
+                    base.OnAttack(sender, e);
+                }
+            }
         }
 
         /// <summary>
